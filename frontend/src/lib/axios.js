64ff42c03a5a043c30147c4988c859a039d7230b
jagadeesh-chinta <chintuchinta25@gmail.com>
2026-04-20 10:28:18 +0530

    frontend ready

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE=="development" ? "http://localhost:3000/api" : "/api",
+    baseURL: "https://chatify-app-v7ci.onrender.com/api",
     withCredentials: true,
 })
